03f21e1b2307241377f42cd8c400dad7b7d47650
samadet <adedokundamilare41@gmail.com>
2022-11-23 23:32:13 +0000

    JQuesry

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,17 @@
+Djkloi462
+A
+B
+
+int = 'https://www.fourtonfish.com/hellosalut/?lang=';
+
+$('document').ready(() => {
+  $('INPUT#btn_translate').click(() => {
+    const lang = $('INPUT#language_code').val();
+    $.get(endpoint + lang, (data) => {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+A
+B
+A
+});
